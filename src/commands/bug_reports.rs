11222cc6f874350973346f6fc173b3c1120b45d5
AldanTanneo <aldantanneo@gmail.com>
2022-02-08 01:29:43 +0100

    updated db and bug commands to fit EoA model

--- src/commands/bug_reports.rs
+++ src/commands/bug_reports.rs
@@ -12,9 +12,9 @@ use crate::check::*;
 use crate::constants::{EOA_DISCORD, MANAGE_BOT_PERMS, OWNER_ID};
 use crate::database::admin_data::is_admin_function;
 use crate::database::bug_reports::{
-    add_bug_report, add_link, add_notified_user, change_bug_status, change_title, get_bug_from_id,
-    get_bug_list, get_bug_statistics, get_notifications_for_user, get_notified_users,
-    is_notified_user, remove_link, switch_edition, BugOrder, BugStatus,
+    add_bug_report, add_link, add_notified_user, change_bug_status, change_category, change_title,
+    get_bug_from_id, get_bug_list, get_bug_statistics, get_notifications_for_user,
+    get_notified_users, is_notified_user, remove_link, BugCategory, BugOrder, BugStatus,
 };
 use crate::failure;
 
@@ -57,11 +57,11 @@ macro_rules! create_bug_embed {
             });
             e.colour($bug.status.colour());
             e.title(format!(
-                "{} LOTR-{}: {}{}",
+                "{} EoA-{}: {} [{}]",
                 $bug.status.marker(),
                 $bug.bug_id,
                 $bug.title,
-                if $bug.legacy { " [legacy]" } else { "" }
+                $bug.category
             ));
             if let Ok(ref message) = $linked_message {
                 e.description(&message.content);
@@ -173,10 +173,7 @@ pub async fn notify_users(
 #[checks(is_admin, is_lotr_discord)]
 #[aliases(report)]
 pub async fn track(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let legacy = args.current().map(|s| s == "legacy").unwrap_or_default();
-    if legacy {
-        args.advance();
-    }
+    let category = args.single::<BugCategory>().unwrap_or_default();
     let status = args.single::<BugStatus>().unwrap_or_default();
 
     let title = args.rest();
@@ -193,7 +190,7 @@ pub async fn track(ctx: &Context, msg: &Message, mut args: Args) -> CommandResul
     };
 
     let bug_id =
-        match add_bug_report(ctx, referenced_message, title.to_string(), status, legacy).await {
+        match add_bug_report(ctx, referenced_message, title.to_string(), status, category).await {
             Ok(bug_id) => bug_id,
             Err(e) => {
                 failure!(ctx, msg, "Could not submit the bug report!");
@@ -204,8 +201,8 @@ pub async fn track(ctx: &Context, msg: &Message, mut args: Args) -> CommandResul
     msg.channel_id
         .send_message(ctx, |m| {
             m.content(format!(
-                "Tracking bug LOTR-{} (priority: `{}`)",
-                bug_id, status
+                "Tracking bug EoA-{} (priority: `{}`) [{}]",
+                bug_id, status, category
             ))
             .reference_message(referenced_message)
             .allowed_mentions(|f| f.empty_parse())
@@ -297,14 +294,14 @@ async fn display_bugs(
     status: Option<BugStatus>,
     limit: u32,
     display_order: BugOrder,
-    legacy: Option<bool>,
+    category: Option<BugCategory>,
     page: u32,
     reply_to: Either<'_>,
 ) -> Result<Option<Message>, SerenityError> {
     assert_ne!(page, 0);
 
     if let Some((bugs, total_bugs)) =
-        get_bug_list(ctx, status, limit, display_order, legacy, page - 1).await
+        get_bug_list(ctx, status, limit, display_order, category, page - 1).await
     {
         if ((page - 1) * limit) >= total_bugs {
             reply_to.failure(ctx, "Page number too high, consider calling `!bugs` and using the navigation arrows.").await?;
@@ -320,14 +317,10 @@ async fn display_bugs(
                 "{} Bug reports (Status: {}){} (Total: {})",
                 status.marker(),
                 status,
-                if let Some(b) = legacy {
-                    if b {
-                        " [legacy]"
-                    } else {
-                        " [renewed]"
-                    }
+                if let Some(c) = category {
+                    format!(" [{c}]")
                 } else {
-                    ""
+                    "".into()
                 },
                 total_bugs
             );
@@ -338,10 +331,10 @@ async fn display_bugs(
                     format!(
                         "{}{}",
                         b,
-                        if legacy.is_none() && b.legacy {
-                            " [legacy]"
+                        if category.is_none() {
+                            format!(" [{}]", b.category)
                         } else {
-                            ""
+                            "".into()
                         }
                     )
                 })
@@ -351,14 +344,10 @@ async fn display_bugs(
         } else {
             title = format!(
                 "Open bug reports{} (Total: {})",
-                if let Some(b) = legacy {
-                    if b {
-                        " [legacy]"
-                    } else {
-                        " [renewed]"
-                    }
+                if let Some(c) = category {
+                    format!(" [{c}]")
                 } else {
-                    ""
+                    "".into()
                 },
                 total_bugs
             );
@@ -370,10 +359,10 @@ async fn display_bugs(
                         "{} {}{}",
                         b.status.marker(),
                         b,
-                        if legacy.is_none() && b.legacy {
-                            " [legacy]"
+                        if category.is_none() {
+                            format!(" [{}]", b.category)
                         } else {
-                            ""
+                            "".into()
                         }
                     )
                 })
@@ -459,17 +448,7 @@ async fn display_bugs(
 #[aliases(bugs)]
 #[sub_commands(bugtracker_help)]
 pub async fn buglist(ctx: &Context, msg: &Message, mut args: Args) -> CommandResult {
-    let legacy = args
-        .current()
-        .map(|s| match s {
-            "legacy" => Some(true),
-            "renewed" => Some(false),
-            _ => None,
-        })
-        .flatten();
-    if legacy.is_some() {
-        args.advance();
-    }
+    let category = args.single::<BugCategory>().ok();
     let status = args.single::<BugStatus>().ok();
 
     let mut display_order = match args.current() {
@@ -500,7 +479,7 @@ pub async fn buglist(ctx: &Context, msg: &Message, mut args: Args) -> CommandRes
         status,
         limit,
         display_order,
-        legacy,
+        category,
         page,
         Either::Message(msg),
     )
@@ -546,7 +525,7 @@ pub async fn buglist(ctx: &Context, msg: &Message, mut args: Args) -> CommandRes
             status,
             limit,
             display_order,
-            legacy,
+            category,
             page,
             Either::Interaction(interaction.as_ref()),
         )
@@ -764,7 +743,7 @@ pub async fn bug_status(ctx: &Context, msg: &Message, mut args: Args) -> Command
     if let Ok(bug_id) = args.single::<String>() {
         if let Ok(bug_id) = bug_id
             .to_uppercase()
-            .trim_start_matches("LOTR-")
+            .trim_start_matches("EoA-")
             .parse::<u64>()
         {
             if let Ok(new_status) = args.single::<BugStatus>() {
@@ -846,7 +825,7 @@ pub async fn bug_close(ctx: &Context, msg: &Message, mut args: Args) -> CommandR
     if let Ok(bug_id) = args.single::<String>() {
         if let Ok(bug_id) = bug_id
             .to_uppercase()
-            .trim_start_matches("LOTR-")
+            .trim_start_matches("EoA-")
             .parse::<u64>()
         {
             if let Err(e) = change_bug_status(ctx, bug_id, BugStatus::Closed).await {
@@ -888,7 +867,7 @@ pub async fn bug_link(ctx: &Context, msg: &Message, mut args: Args) -> CommandRe
                     return Ok(());
                 }
                 if let Some(link_id) = add_link(ctx, bug_id, &message.link(), title).await {
-                    termite_success!(ctx, msg, "Added link #{} to LOTR-{}", link_id, bug_id);
+                    termite_success!(ctx, msg, "Added link #{} to EoA-{}", link_id, bug_id);
                     notify_users(
                         ctx,
                         bug_id,
@@ -909,7 +888,7 @@ pub async fn bug_link(ctx: &Context, msg: &Message, mut args: Args) -> CommandRe
                     return Ok(());
                 }
                 if let Some(link_id) = add_link(ctx, bug_id, &link, title).await {
-                    termite_success!(ctx, msg, "Added link #{} to LOTR-{}", link_id, bug_id);
+                    termite_success!(ctx, msg, "Added link #{} to EoA-{}", link_id, bug_id);
                     notify_users(
                         ctx,
                         bug_id,
@@ -983,36 +962,38 @@ pub async fn bug_toggle_edition(ctx: &Context, msg: &Message, mut args: Args) ->
     if let Ok(bug_id) = args.single::<String>() {
         if let Ok(bug_id) = bug_id
             .to_uppercase()
-            .trim_start_matches("LOTR-")
+            .trim_start_matches("EoA-")
             .parse::<u64>()
         {
-            if let Some(legacy) = switch_edition(ctx, bug_id).await {
-                termite_success!(
+            if let Ok(category) = args.single::<BugCategory>() {
+                if let Some(old_category) = change_category(ctx, bug_id, category).await {
+                    termite_success!(
+                        ctx,
+                        msg,
+                        "EoA-{} has been changed from {} to {}",
+                        bug_id,
+                        old_category,
+                        category
+                    );
+                    notify_users(
+                        ctx,
+                        bug_id,
+                        format!(
+                            "A bug you are subscribed to has been changed from {} to {}",
+                            old_category, category
+                        ),
+                    )
+                    .await?;
+                } else {
+                    failure!(ctx, msg, "The bug EoA-{} does not exist!", bug_id);
+                }
+            } else {
+                failure!(
                     ctx,
                     msg,
-                    "EoA-{} has been changed from {}",
-                    bug_id,
-                    if legacy {
-                        "renewed to legacy"
-                    } else {
-                        "legacy to renewed"
-                    }
-                );
-                notify_users(
-                    ctx,
-                    bug_id,
-                    format!(
-                        "A bug you are subscribed to has been changed from {}",
-                        if legacy {
-                            "renewed to legacy"
-                        } else {
-                            "legacy to renewed"
-                        }
-                    ),
+                    "The first argument must be one of \
+`fa_renewed`, `fa_legacy`, `sa_renewed` or `sa_legacy`"
                 )
-                .await?;
-            } else {
-                failure!(ctx, msg, "The bug EoA-{} does not exist!", bug_id);
             }
         } else {
             failure!(ctx, msg, "`{}` is not a valid bug id!", bug_id);
@@ -1037,12 +1018,7 @@ pub async fn bug_rename(ctx: &Context, msg: &Message, mut args: Args) -> Command
             if new_title.is_empty() {
                 failure!(ctx, msg, "You must specify a new title for EoA-{}", bug_id);
             } else if change_title(ctx, bug_id, new_title).await.is_ok() {
-                termite_success!(
-                    ctx,
-                    msg,
-                    "Successfully changed the title of EoA-{}",
-                    bug_id
-                );
+                termite_success!(ctx, msg, "Successfully changed the title of EoA-{}", bug_id);
                 notify_users(
                     ctx,
                     bug_id,
@@ -1064,9 +1040,7 @@ pub async fn bug_rename(ctx: &Context, msg: &Message, mut args: Args) -> Command
 #[command]
 #[aliases(statistics)]
 pub async fn stats(ctx: &Context, msg: &Message) -> CommandResult {
-    if let Some([resolved, low, medium, high, critical, closed, forgevanilla, total, legacy]) =
-        get_bug_statistics(ctx).await
-    {
+    if let Some(counts) = get_bug_statistics(ctx).await {
         msg.channel_id
             .send_message(ctx, |m| {
                 m.embed(|e| {
@@ -1090,18 +1064,19 @@ _Open bugs: {}_
 {} critical bugs
 
 **Total: {} tracked bugs**
-\t_including {} legacy bugs_
+_{} first age bugs, {} second age bugs_
 ",
-                            resolved,
-                            closed,
-                            forgevanilla,
-                            total - resolved - closed - forgevanilla,
-                            low,
-                            medium,
-                            high,
-                            critical,
-                            total,
-                            legacy,
+                            counts.resolved,
+                            counts.closed,
+                            counts.forgevanilla,
+                            counts.total - counts.resolved - counts.closed - counts.forgevanilla,
+                            counts.low,
+                            counts.medium,
+                            counts.high,
+                            counts.critical,
+                            counts.total,
+                            counts.total - counts.second_age,
+                            counts.second_age
                         ),
                         false,
                     );
@@ -1157,7 +1132,7 @@ including those from closed or resolved bugs."
                     .description(format!(
                         "_List of bugs you are subscribed to_\n\n{}",
                         list.iter()
-                            .map(|id| format!("LOTR-{id}"))
+                            .map(|id| format!("EoA-{id}"))
                             .collect::<Vec<_>>()
                             .join(", "),
                     ))
@@ -1195,7 +1170,7 @@ pub async fn unsubscribe(ctx: &Context, msg: &Message, mut args: Args) -> Comman
     if let Err(e) =
         crate::database::bug_reports::remove_notified_user(ctx, bug_id, msg.author.id).await
     {
-        failure!(ctx, msg, "Could not unsubscribe from LOTR-{}", bug_id);
+        failure!(ctx, msg, "Could not unsubscribe from EoA-{}", bug_id);
         return Err(e);
     }
 
--- src/database/bug_reports.rs
+++ src/database/bug_reports.rs
@@ -18,6 +18,63 @@ pub enum BugOrder {
     None,
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum BugCategory {
+    FaLegacy,
+    FaRenewed,
+    SaLegacy,
+    SaRenewed,
+}
+
+impl std::fmt::Display for BugCategory {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        use BugCategory::*;
+        match self {
+            FaLegacy => write!(f, "FA Legacy"),
+            FaRenewed => write!(f, "FA Renewed"),
+            SaLegacy => write!(f, "SA Legacy"),
+            SaRenewed => write!(f, "SA Renewed"),
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct ParseCategoryError;
+
+impl std::str::FromStr for BugCategory {
+    type Err = ParseCategoryError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        use BugCategory::*;
+        Ok(match s.to_ascii_lowercase().as_str() {
+            "fa_legacy" => FaLegacy,
+            "fa_renewed" => FaRenewed,
+            "sa_legacy" => SaLegacy,
+            "sa_renewed" => SaRenewed,
+            _ => Err(ParseCategoryError)?,
+        })
+    }
+}
+
+impl BugCategory {
+    pub fn as_str(self) -> &'static str {
+        use BugCategory::*;
+
+        match self {
+            FaLegacy => "fa_legacy",
+            FaRenewed => "fa_renewed",
+            SaLegacy => "sa_legacy",
+            SaRenewed => "sa_renewed",
+        }
+    }
+}
+
+impl Default for BugCategory {
+    fn default() -> BugCategory {
+        BugCategory::FaRenewed
+    }
+}
+
 use BugStatus::*;
 
 #[derive(Debug, Copy, Clone, PartialEq, Eq)]
@@ -40,23 +97,23 @@ impl std::fmt::Display for BugStatus {
     }
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Copy)]
 pub struct ParseStatusError;
 
 impl std::str::FromStr for BugStatus {
     type Err = ParseStatusError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s.to_lowercase().as_str() {
-            "resolved" => Ok(Resolved),
-            "low" => Ok(Low),
-            "medium" => Ok(Medium),
-            "high" => Ok(High),
-            "critical" => Ok(Critical),
-            "closed" => Ok(Closed),
-            "forgevanilla" | "forge" | "vanilla" => Ok(ForgeVanilla),
-            _ => Err(Self::Err {}),
-        }
+        Ok(match s.to_lowercase().as_str() {
+            "resolved" => Resolved,
+            "low" => Low,
+            "medium" => Medium,
+            "high" => High,
+            "critical" => Critical,
+            "closed" => Closed,
+            "forgevanilla" | "forge" | "vanilla" => ForgeVanilla,
+            _ => Err(Self::Err {})?,
+        })
     }
 }
 
@@ -68,7 +125,7 @@ impl std::default::Default for BugStatus {
 
 impl BugStatus {
     #[inline]
-    pub const fn as_str(&self) -> &str {
+    pub const fn as_str(self) -> &'static str {
         match self {
             Resolved => "resolved",
             Low => "low",
@@ -81,7 +138,7 @@ impl BugStatus {
     }
 
     #[inline]
-    pub const fn colour(&self) -> Colour {
+    pub const fn colour(self) -> Colour {
         match self {
             Resolved => Colour::new(0x2fd524),
             Low => Colour::new(0xfef001),
@@ -93,7 +150,7 @@ impl BugStatus {
         }
     }
 
-    pub const fn marker(&self) -> &str {
+    pub const fn marker(self) -> &'static str {
         match self {
             Resolved => ":green_circle:",
             Low => ":yellow_circle:",
@@ -105,7 +162,7 @@ impl BugStatus {
         }
     }
 
-    pub fn reaction(&self) -> ReactionType {
+    pub fn reaction(self) -> ReactionType {
         ReactionType::Unicode(
             match self {
                 Resolved => "✅",
@@ -139,7 +196,7 @@ pub struct BugReport {
     pub title: String,
     pub status: BugStatus,
     pub timestamp: DateTime<Utc>,
-    pub legacy: bool,
+    pub category: BugCategory,
     pub links: Vec<BugLink>,
 }
 
@@ -149,7 +206,7 @@ pub struct PartialBugReport {
     pub title: String,
     pub status: BugStatus,
     pub timestamp: DateTime<Utc>,
-    pub legacy: bool,
+    pub category: BugCategory,
 }
 
 impl std::fmt::Display for PartialBugReport {
@@ -175,14 +232,14 @@ impl PartialBugReport {
         title: String,
         status: String,
         timestamp: NaiveDateTime,
-        legacy: bool,
+        category: BugCategory,
     ) -> Option<Self> {
         Some(Self {
             bug_id,
             title,
             status: status.parse().unwrap_or_default(),
             timestamp: DateTime::from_utc(timestamp, Utc),
-            legacy,
+            category,
         })
     }
 }
@@ -190,17 +247,17 @@ impl PartialBugReport {
 pub async fn get_bug_from_id(ctx: &Context, bug_id: u64) -> Result<BugReport, CommandError> {
     let mut conn = get_database_conn!(ctx);
 
-    let (channel_id, message_id, title, status, timestamp, legacy): (
+    let (channel_id, message_id, title, status, timestamp, category): (
         u64,
         u64,
         String,
         String,
         NaiveDateTime,
-        bool,
+        String,
     ) = conn
         .exec_first(
             formatcp!(
-                "SELECT channel_id, message_id, title, status, timestamp, legacy \
+                "SELECT channel_id, message_id, title, status, timestamp, category \
 FROM {} WHERE bug_id = :bug_id",
                 TABLE_BUG_REPORTS
             ),
@@ -230,11 +287,12 @@ FROM {} WHERE bug_id = :bug_id",
         message_id: MessageId(message_id),
         title,
         status: status
-            .as_str()
             .parse()
             .expect("Expected a valid bug status from the database"),
         timestamp: DateTime::from_utc(timestamp, Utc),
-        legacy,
+        category: category
+            .parse()
+            .expect("Expected a valid bug category from the database"),
         links,
     })
 }
@@ -244,14 +302,14 @@ pub async fn add_bug_report(
     msg: &Message,
     title: String,
     status: BugStatus,
-    legacy: bool,
+    category: BugCategory,
 ) -> Result<u64, CommandError> {
     let mut conn = get_database_conn!(ctx);
 
     conn.exec_drop(
         formatcp!(
-            "INSERT INTO {} (channel_id, message_id, title, status, legacy) \
-VALUES (:channel_id, :message_id, :title, :status, :legacy)",
+            "INSERT INTO {} (channel_id, message_id, title, status, category) \
+VALUES (:channel_id, :message_id, :title, :status, :category)",
             TABLE_BUG_REPORTS
         ),
         params! {
@@ -259,7 +317,7 @@ VALUES (:channel_id, :message_id, :title, :status, :legacy)",
             "message_id" => msg.id.0,
             "title" => title,
             "status" => status.as_str(),
-            "legacy" => legacy,
+            "category" => category.as_str(),
         },
     )
     .await?;
@@ -278,22 +336,22 @@ pub async fn get_bug_list(
     status: Option<BugStatus>,
     limit: u32,
     display_order: BugOrder,
-    legacy: Option<bool>,
+    category: Option<BugCategory>,
     page: u32,
 ) -> Option<(Vec<PartialBugReport>, u32)> {
     let mut conn = get_database_conn!(ctx);
 
     let total: u32 = conn
         .query_first(format!(
-            "SELECT COUNT(bug_id) FROM {} WHERE {} {legacy}",
+            "SELECT COUNT(bug_id) FROM {} WHERE {} {category}",
             TABLE_BUG_REPORTS,
             if let Some(status) = status {
                 format!("status = '{}'", status.as_str())
             } else {
                 "status != 'resolved' AND status != 'closed' AND status != 'forgevanilla'".into()
             },
-            legacy = if let Some(b) = legacy {
-                format!("AND legacy = {}", b as u8)
+            category = if let Some(c) = category {
+                format!("AND category = '{}'", c.as_str())
             } else {
                 "".into()
             },
@@ -304,15 +362,15 @@ pub async fn get_bug_list(
     conn.exec_map(
         format!(
             "SELECT bug_id, title, status, timestamp, legacy FROM {} \
-WHERE {} {legacy} ORDER BY {ordering} LIMIT :limit OFFSET :offset",
+WHERE {} {category} ORDER BY {ordering} LIMIT :limit OFFSET :offset",
             TABLE_BUG_REPORTS,
             if let Some(status) = status {
                 format!("status = '{}'", status.as_str())
             } else {
                 "status != 'resolved' AND status != 'closed' AND status != 'forgevanilla'".into()
             },
-            legacy = if let Some(b) = legacy {
-                format!("AND legacy = {}", b as u8)
+            category = if let Some(c) = category {
+                format!("AND category = '{}'", c.as_str())
             } else {
                 "".into()
             },
@@ -327,7 +385,13 @@ WHERE {} {legacy} ORDER BY {ordering} LIMIT :limit OFFSET :offset",
             "limit" => limit,
             "offset" => limit * page
         },
-        |(bug_id, title, status, timestamp, legacy): (u64, String, String, NaiveDateTime, bool)| {
+        |(bug_id, title, status, timestamp, category): (
+            u64,
+            String,
+            String,
+            NaiveDateTime,
+            String,
+        )| {
             PartialBugReport::new(
                 bug_id,
                 title,
@@ -335,7 +399,9 @@ WHERE {} {legacy} ORDER BY {ordering} LIMIT :limit OFFSET :offset",
                     .parse()
                     .expect("Expected a valid bug status from the database"),
                 timestamp,
-                legacy,
+                category
+                    .parse()
+                    .expect("Expected a valid bug category from the database"),
             )
         },
     )
@@ -460,7 +526,20 @@ pub async fn change_title(ctx: &Context, bug_id: u64, new_title: &str) -> Comman
     Ok(())
 }
 
-pub async fn get_bug_statistics(ctx: &Context) -> Option<[u32; 9]> {
+#[derive(Debug, Clone, Copy)]
+pub struct Counts {
+    pub resolved: u32,
+    pub low: u32,
+    pub medium: u32,
+    pub high: u32,
+    pub critical: u32,
+    pub closed: u32,
+    pub forgevanilla: u32,
+    pub total: u32,
+    pub second_age: u32,
+}
+
+pub async fn get_bug_statistics(ctx: &Context) -> Option<Counts> {
     let mut conn = get_database_conn!(ctx);
 
     let statuses = [
@@ -495,41 +574,61 @@ pub async fn get_bug_statistics(ctx: &Context) -> Option<[u32; 9]> {
 
     counts[8] = conn
         .query_first(formatcp!(
-            "SELECT COUNT(bug_id) FROM {} WHERE legacy = 1",
+            "SELECT COUNT(bug_id) FROM {} WHERE category = 'sa_legacy' OR category = 'sa_renewed'",
             TABLE_BUG_REPORTS
         ))
         .await
         .ok()??;
 
-    Some(counts)
+    Some(Counts {
+        resolved: counts[0],
+        low: counts[1],
+        medium: counts[2],
+        high: counts[3],
+        critical: counts[4],
+        closed: counts[5],
+        forgevanilla: counts[6],
+        total: counts[7],
+        second_age: counts[8],
+    })
 }
 
-pub async fn switch_edition(ctx: &Context, bug_id: u64) -> Option<bool> {
+pub async fn change_category(
+    ctx: &Context,
+    bug_id: u64,
+    category: BugCategory,
+) -> Option<BugCategory> {
     let mut conn = get_database_conn!(ctx);
+
+    let old_category = conn
+        .exec_first::<String, _, _>(
+            formatcp!(
+                "SELECT category FROM {} WHERE bug_id = :bug_id",
+                TABLE_BUG_REPORTS
+            ),
+            params! {
+                "bug_id" => bug_id
+            },
+        )
+        .await
+        .ok()??
+        .parse::<BugCategory>()
+        .expect("Expected a valid bug category from the database");
 
     conn.exec_drop(
         formatcp!(
-            "UPDATE {} SET legacy = NOT legacy WHERE bug_id = :bug_id",
+            "UPDATE {} SET category = :category WHERE bug_id = :bug_id",
             TABLE_BUG_REPORTS
         ),
         params! {
+            "category" => category.as_str(),
             "bug_id" => bug_id
         },
     )
     .await
     .ok()?;
 
-    conn.exec_first(
-        formatcp!(
-            "SELECT legacy FROM {} WHERE bug_id = :bug_id",
-            TABLE_BUG_REPORTS
-        ),
-        params! {
-            "bug_id" => bug_id,
-        },
-    )
-    .await
-    .ok()?
+    Some(old_category)
 }
 
 pub async fn is_notified_user(ctx: &Context, bug_id: u64, user_id: UserId) -> Option<bool> {
